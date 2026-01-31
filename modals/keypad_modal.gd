f00b6314279e533799902c7b3fb5019093b35559
cripsy <152404915+enchong-codes@users.noreply.github.com>
2026-01-31 17:00:07 -0600

    modal functions added

--- modals/keypad_modal.gd
+++ modals/keypad_modal.gd
@@ -1,9 +1,40 @@
-extends Control
+class_name InputModal extends Control
 
+
+@onready var header_label: Label = $Modal/MarginContainer/VBoxContainer/HeaderLabel
+@onready var message_label: Label = $Modal/MarginContainer/VBoxContainer/MessageLabel
 @onready var line_edit: LineEdit = $Modal/MarginContainer/VBoxContainer/HBoxContainer/LineEdit
+@onready var cancel_button: Button = $Modal/MarginContainer/VBoxContainer/HBoxContainer2/CancelButton
 
-func _ready():
-	line_edit.text_submitted.connect(_on_LineEdit_text_entered)
+var is_open: bool = false
+
+
+func _ready() -> void:
+	set_process_unhandled_key_input(false)
+	
+	if line_edit:
+		line_edit.text_submitted.connect(_on_LineEdit_text_entered)
+	
+	if cancel_button:
+		cancel_button.pressed.connect(_on_cancel_button_pressed)
+	
+	hide()
+	
+func _unhandled_key_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		cancel()
+	
+func cancel() -> void:
+	_close_modal(false)	
+
+func _close_modal(is_confirmed: bool) -> void:
+	set_process_unhandled_key_input(false)
+	set_deferred("is_open", false)
+	hide()
 	
 func _on_LineEdit_text_entered(new_text: String) -> void:
+	_close_modal(true)
+	
+func _on_cancel_button_pressed() -> void:
 	pass
+	
