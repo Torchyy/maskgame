4ee815a6c215ca7e7b0616bc2fcb356571aed19c
Devanush <devanushjoseph@gmail.com>
2026-01-30 22:24:23 -0600

    Initial commit

---
+++ Player/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle interact
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		pass
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	velocity = Input.get_vector("left", "right", "up", "down")
+	velocity.normalized()
+	velocity *= SPEED
+	#if direction.x == 0:
+		#velocity.x = direction.x * SPEED
+	#else:d
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
