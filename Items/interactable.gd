97da6e0844f38decc09203d6934cd0d9b4997ef7
Devanush <devanushjoseph@gmail.com>
2026-01-30 22:47:48 -0600

    created Interactable interface

---
+++ Items/interactable.gd
@@ -0,0 +1,9 @@
+class_name Interactable
+extends Node2D
+
+func execute() -> void:
+	print(
+			"Interacting with ", self,
+			", IF YOU ARE SEEING THIS THEN
+			THIS FUNCTION HAS NOT BEEN OVERWRITTEN"
+		)
