27564774fb4ca56104333ea2e3a33ba3ff648674
Devanush Joseph <102316343+Torchyy@users.noreply.github.com>
2026-02-01 15:39:51 -0600

    Hub is now connected

--- levels/level_main.gd
+++ levels/level_main.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var Camera : Camera2D
 @export var User : Player
+@export var Audio : AudioStreamPlayer2D
 
 func _ready() -> void:
 	PlayerStats.reset()
@@ -15,16 +16,10 @@ func _teleport_hub_to_war_entered(body: Node2D) -> void:
 	Camera.global_position = $WarRoom.global_position
 	User.global_position.x = $MarkerWar.global_position.x
 
-
 func _teleport_war_to_hub_entered(body: Node2D) -> void:
 	camera_hub()
 	User.global_position.x = $MarkerWarToHub.global_position.x
 
-func _teleport_hub_to_death_entered(body: Node2D) -> void:
-	pass # Replace with function body.
-
-func teleport_hub_to_famine_entered(body: Node2D) -> void:
-	pass # Replace with function body.
 
 func teleport_hub_to_conquest_entered(body: Node2D) -> void:
 	Camera.global_position = $ConquestRoom.global_position
@@ -33,3 +28,21 @@ func teleport_hub_to_conquest_entered(body: Node2D) -> void:
 func _teleport_conquest_to_hub_entered(body: Node2D) -> void:
 	camera_hub()
 	User.global_position.y = $MarkerConquestToHub.global_position.y
+
+
+func _teleport_hub_to_death_entered(body: Node2D) -> void:
+	Camera.global_position = $DeathRoom.global_position
+	User.global_position.x = $MarkerDeath.global_position.x
+
+func teleport_death_to_hub_entered(body: Node2D) -> void:
+	camera_hub()
+	User.global_position.x = $MarkerDeathToHub.global_position.x
+
+
+func teleport_famine_to_hub_entered(body: Node2D) -> void:
+	camera_hub()
+	User.global_position.y = $MarkerFamineToHub.global_position.y
+
+func teleport_hub_to_famine_entered(body: Node2D) -> void:
+	Camera.global_position = $FamineRoom.global_position
+	User.global_position.y = $MarkerFamine.global_position.y
